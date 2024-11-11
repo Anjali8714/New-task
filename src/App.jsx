b97d40e68abd91adb9cd1faf71b924977cce19eb
Anjalit9714 <anjalit9714@gmail.com>
2024-11-11 16:18:50 +0530

    new work

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Component1 from './Components/Component1'
+import Context from './Components/Context'
+import Component2 from './Components/Component2'
+import Component3 from './Components/Component3'
+
+
+const App = () => {
+  return (
+    <div>
+     
+       
+        <Component1/>
+        <Component2/>
+        <Component3/>
+        
+     
+    </div>
+  )
+}
+
+export default App
+
